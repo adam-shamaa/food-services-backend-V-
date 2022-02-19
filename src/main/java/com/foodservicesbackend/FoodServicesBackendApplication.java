1642962f1f8e6ab1e8a49abeb282014a957edc14
adamshamaa <adamshamaa@gmail.com>
2022-02-19 15:50:43 -0500

    and a repository was born...

---
+++ src/main/java/com/foodservicesbackend/FoodServicesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.foodservicesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodServicesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodServicesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foodservicesbackend/FoodServicesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodservicesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodServicesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
