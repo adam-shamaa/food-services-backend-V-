d106d406afba2fb13d49c79b262483549941c29a
adamshamaa <adamshamaa@gmail.com>
2022-04-15 17:29:51 -0400

    fix: netty http client radnomy prematurely closing connection

--- src/main/java/com/foodservicesapi/services/skipthedishes/SkipTheDishesClient.java
+++ src/main/java/com/foodservicesapi/services/skipthedishes/SkipTheDishesClient.java
@@ -4,69 +4,59 @@ import com.foodservicesapi.codegen.models.RestaurantDetailsRequestSkipTheDishes;
 import com.foodservicesapi.codegen.models.RestaurantDetailsResponseWrapperSkipTheDishes;
 import com.foodservicesapi.codegen.models.RestaurantsListRequestWrapperSkipTheDishes;
 import com.foodservicesapi.codegen.models.RestaurantsListResponseWrapperSkipTheDishes;
-import io.netty.handler.logging.LogLevel;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.apache.http.client.utils.URIBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.transport.logging.AdvancedByteBufFormat;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
 public class SkipTheDishesClient {
 
-  private final WebClient webClient;
+  private final RestTemplate webClient;
 
   public SkipTheDishesClient() {
-    this.webClient =
-        WebClient.builder()
-            .exchangeStrategies(
-                ExchangeStrategies.builder()
-                    .codecs(
-                        configurer -> configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024))
-                    .build())
-            .build();
+    this.webClient = new RestTemplate();
   }
 
   public RestaurantsListResponseWrapperSkipTheDishes fetchRestaurantsLists(
       RestaurantsListRequestWrapperSkipTheDishes requestBody, String searchQuery) {
     return this.webClient
-        .post()
-        .uri("https://api.skipthedishes.com/customer/v1/graphql")
-        .accept(MediaType.APPLICATION_JSON)
-        .header("App-Token", "d7033722-4d2e-4263-9d67-d83854deb0fc")
-        .header("Parameters", "isCuisineSearch=false&isSorted=false&search=" + searchQuery)
-        .body(Mono.just(requestBody), RestaurantsListRequestWrapperSkipTheDishes.class)
-        .retrieve()
-        .bodyToMono(RestaurantsListResponseWrapperSkipTheDishes.class)
-        .block();
+            .exchange("https://api.skipthedishes.com/customer/v1/graphql",
+                    HttpMethod.POST,
+                    new HttpEntity(requestBody, new HttpHeaders(){{
+                      set("App-Token", "d7033722-4d2e-4263-9d67-d83854deb0fc");
+                      set("Parameters", "isCuisineSearch=false&isSorted=false&search=" + searchQuery);
+                    }}),
+                    RestaurantsListResponseWrapperSkipTheDishes.class
+                  )
+            .getBody();
   }
 
+  @SneakyThrows
   public RestaurantDetailsResponseWrapperSkipTheDishes fetchRestaurantDetails(
       RestaurantDetailsRequestSkipTheDishes request) {
     return this.webClient
-        .get()
-        .uri(
-            uriBuilder ->
-                uriBuilder
-                    .scheme("https")
-                    .host("api-skipthedishes.skipthedishes.com")
-                    .path("/v1/restaurants/clean-url/")
-                    .path(request.getId())
-                    .queryParam("fullMenu", false)
-                    .queryParam("language", "en")
-                    .queryParam("lat", request.getLatitude())
-                    .queryParam("long", request.getLongitude())
-                    .queryParam("order_type", "DELIVERY")
-                    .build())
-        .accept(MediaType.APPLICATION_JSON)
-        .header("App-Token", "d7033722-4d2e-4263-9d67-d83854deb0fc")
-        .retrieve()
-        .bodyToMono(RestaurantDetailsResponseWrapperSkipTheDishes.class)
-        .block();
+            .exchange(
+                    new URIBuilder()
+                      .setScheme("https")
+                      .setHost("api-skipthedishes.skipthedishes.com")
+                      .setPath("/v1/restaurants/clean-url/" + request.getId())
+                      .addParameter("fullMenu", "false")
+                      .addParameter("language", "en")
+                      .addParameter("lat", request.getLatitude().toString())
+                      .addParameter("long", request.getLongitude().toString())
+                      .addParameter("order_type", "DELIVERY")
+                      .build(),
+                    HttpMethod.GET,
+                    new HttpEntity(new HttpHeaders(){{
+                      set("App-Token", "d7033722-4d2e-4263-9d67-d83854deb0fc");
+                    }}),
+                    RestaurantDetailsResponseWrapperSkipTheDishes.class
+            ).getBody();
   }
 }
--- src/main/java/com/foodservicesapi/services/ubereats/UberEatsClient.java
+++ src/main/java/com/foodservicesapi/services/ubereats/UberEatsClient.java
@@ -5,9 +5,14 @@ import com.foodservicesapi.codegen.models.*;
 import io.netty.handler.logging.LogLevel;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -19,57 +24,43 @@ import java.net.URLEncoder;
 @Slf4j
 @Component
 public class UberEatsClient {
-  private final WebClient webClient;
+  private final RestTemplate webClient;
 
   public UberEatsClient() {
-    this.webClient =
-        WebClient.builder()
-            .baseUrl("https://www.ubereats.com/api/")
-            .exchangeStrategies(
-                ExchangeStrategies.builder()
-                    .codecs(
-                        configurer -> configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024))
-                    .build())
-            .build();
+    this.webClient = new RestTemplate();
   }
 
   @SneakyThrows
   public RestaurantsListResponseWrapperUbereats fetchRestaurantsLists(
-      RestaurantsListRequestCookieWrapperUbereats requestCookie,
-      RestaurantsListRequestBodyWrapperUbereats requestBody) {
+          RestaurantsListRequestCookieWrapperUbereats requestCookie,
+          RestaurantsListRequestBodyWrapperUbereats requestBody) {
     return this.webClient
-        .post()
-        .uri("getFeedV1")
-        .accept(MediaType.APPLICATION_JSON)
-        .header("x-csrf-token", "x")
-        .header(
-            "Cookie",
-            String.format(
-                "uev2.loc=%s",
-                URLEncoder.encode(new ObjectMapper().writeValueAsString(requestCookie), "UTF-8")))
-        .body(Mono.just(requestBody), RestaurantsListResponseWrapperUbereats.class)
-        .retrieve()
-        .bodyToMono(RestaurantsListResponseWrapperUbereats.class)
-        .block();
+            .exchange(
+                    "https://www.ubereats.com/api/getFeedV1",
+                    HttpMethod.POST,
+                    new HttpEntity<>(requestBody, new HttpHeaders() {{
+                      set("x-csrf-token", "x");
+                      set("Cookie", String.format("uev2.loc=%s", URLEncoder.encode(new ObjectMapper().writeValueAsString(requestCookie), "UTF-8")));
+                    }}),
+                    RestaurantsListResponseWrapperUbereats.class
+            ).getBody();
   }
 
   @SneakyThrows
   public RestaurantDetailsResponseWrapperUbereats fetchRestaurantDetails(
-      RestaurantDetailsRequestCookieUbereats requestCookie,
-      RestaurantDetailsRequestBodyUbereats requestBody) {
+          RestaurantDetailsRequestCookieUbereats requestCookie,
+          RestaurantDetailsRequestBodyUbereats requestBody) {
     return this.webClient
-        .post()
-        .uri("https://www.ubereats.com/api/getStoreV1?localeCode=ca")
-        .accept(MediaType.APPLICATION_JSON)
-        .header("x-csrf-token", "x")
-        .header(
-            "Cookie",
-            String.format(
-                "uev2.loc=%s",
-                URLEncoder.encode(new ObjectMapper().writeValueAsString(requestCookie), "UTF-8")))
-        .body(Mono.just(requestBody), RestaurantDetailsRequestBodyUbereats.class)
-        .retrieve()
-        .bodyToMono(RestaurantDetailsResponseWrapperUbereats.class)
-        .block();
+            .exchange("https://www.ubereats.com/api/getStoreV1?localeCode=ca",
+                    HttpMethod.POST,
+                    new HttpEntity<>(requestBody, new HttpHeaders() {{
+                      set("x-csrf-token", "x");
+                      set("Cookie",
+                              String.format(
+                                      "uev2.loc=%s",
+                                      URLEncoder.encode(new ObjectMapper().writeValueAsString(requestCookie), "UTF-8")));
+                    }}),
+                    RestaurantDetailsResponseWrapperUbereats.class
+                    ).getBody();
   }
 }
